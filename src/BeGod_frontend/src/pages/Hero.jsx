30c442805f8fa5dd3be928ddc4876f66f5a27bae
bhargav-quadbtech <bhargav.vermavathsavai@quadbtech.com>
2024-09-24 15:44:17 +0530

    resolving conflits

--- src/BeGod_frontend/src/pages/Hero.jsx
+++ src/BeGod_frontend/src/pages/Hero.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
-import Navbar from "../components/Landing Page Components/Navbar";
-import YellowButton from "../components/button/YellowButton";
-import { Link } from "react-router-dom";
-import Footer from "../components/Footer";
-import NFTGallery from "../components/Landing Page Components/NftGallery";
-import HeroSlider from "../components/Landing Page Components/HeroSlider";
-import Collections from "../components/Landing Page Components/CollectionType";
-import { useTranslation } from "react-i18next";
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Landing Page Components/Navbar'
+import YellowButton from '../components/button/YellowButton'
+import { Link } from 'react-router-dom';
+import Footer from '../components/Footer';
+import NFTGallery from '../components/Landing Page Components/NftGallery';
+import HeroSlider from '../components/Landing Page Components/HeroSlider';
+import Collections from '../components/Landing Page Components/CollectionType';
+import { useTranslation } from 'react-i18next';
 import { useAuth } from "../utils/useAuthClient.jsx";
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
@@ -64,7 +64,8 @@ const Hero = () => {
     const {t} =  useTranslation();
     const {mainHeading,description,button} = t("landingPage");
 
-  const { backendActor } = useAuth();
+    const { backendActor } = useAuth();
+
 
     useEffect(() => {
         getCollections();
@@ -141,32 +142,26 @@ const getCollectionNfts = (collectionList,collectionId) => {
 
 console.log("current collection list",selectedCollectionNftCardsList)
 
-  return (
-    // for medium devices width is 99.6% because in ipad air width is little overflowing
-    <div className="w-[100%] md:w-[99.6%] lg:w-[100%] font-caslon">
-      <div className="relative">
-        <HeroSlider />
-        <div className=" absolute top-0 w-[100%] z-20">
-          <Navbar mobileView={mobileViewHandler} />
-        </div>
-        <div
-          className={`w-full flex items-center justify-center flex-col space-y-4 pb-8 absolute top-60 ${
-            mobileView ? "z-0" : "z-10"
-          }`}
-        >
-          <h1 className="text-[40px] md:text-[80px] xl:text-[100px] 2xl:text-[128px] flex items-center justify-center h-[50px] md:h-[130px] xl:h-[160px] 2xl:h-[180px] leading-none font-[500] text-transparent bg-clip-text bg-gradient-to-r from-[#FBCEA0] via-[#FFF9F2] to-[#FBCEA0] custom-text-border text-center big-heading">
-            {mainHeading}
-          </h1>
-          <h2 className="text-[16px] md:text-[24px] leading-tight font-[500] text-transparent bg-clip-text bg-gradient-to-r from-[#FBCEA0] via-[#FFF9F2] to-[#FBCEA0] text-center">
-            {description}
-          </h2>
-          <div className="p-2 border-[1px] border-[#FCD37B]">
-            <a href="#collections">
-              <YellowButton>{button}</YellowButton>
-            </a>
-          </div>
-        </div>
-      </div>
+    return (
+        // for medium devices width is 99.6% because in ipad air width is little overflowing
+        <div className='w-[100%] md:w-[99.6%] lg:w-[100%] font-caslon'>
+            <div className='relative'>
+                <HeroSlider />
+                <div className=' absolute top-0 w-[100%] z-20'>
+                    <Navbar mobileView={mobileViewHandler}/>
+                </div>
+                <div  className={`w-full flex items-center justify-center flex-col space-y-8 py-8 absolute top-60 ${mobileView?"z-0":"z-10"}`}>
+                    <h1  className="text-[40px] md:text-[80px] xl:text-[100px] 2xl:text-[128px] flex items-center justify-center h-[50px] md:h-[130px] xl:h-[160px] 2xl:h-[180px] leading-none font-[500] text-transparent bg-clip-text bg-gradient-to-r from-[#FBCEA0] via-[#FFF9F2] to-[#FBCEA0] custom-text-border text-center">
+                        {mainHeading}
+                    </h1>
+                    <h2 className='text-[16px] md:text-[24px] leading-tight font-[500] text-transparent bg-clip-text bg-gradient-to-r from-[#FBCEA0] via-[#FFF9F2] to-[#FBCEA0] text-center'>
+                      {description}
+                    </h2>
+                    <div className='p-2 border-[1px] border-[#FCD37B]'>
+                        <a href='#collections'><YellowButton>{button}</YellowButton></a>
+                    </div>
+                </div>
+            </div>
 
             <div id='collections' style={{ backgroundImage: `url('/Hero/smoke 1.png')`, backgroundRepeat: "no-repeat" }} className='h-auto w-[100%] bg-cover bg-center'>
                 <div  className='max-w-[1920px] mx-auto pt-12 flex flex-col items-center justify-center'>
@@ -286,4 +281,4 @@ console.log("current collection list",selectedCollectionNftCardsList)
     )
 }
 
-export default Hero;
+export default Hero
