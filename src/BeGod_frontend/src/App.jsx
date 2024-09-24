db242bcf182d7d55887d7901a978bd58f9e32882
codesclown <chandan.vishwakarma@quadbtech.com>
2024-09-24 17:11:36 +0530

    change in code

--- src/BeGod_frontend/src/App.jsx
+++ src/BeGod_frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Admin from './Admin/Admin';
 import Login from './Admin/Login';
@@ -8,19 +8,24 @@ import NftDetails from './components/NftDetails';
 import Hero from './pages/Hero';
 import BuyNft from './pages/BuyNft';
 import PageNotFound from './Admin/PageNotFound';
+const FullpageLoader = lazy(() => import('./Loader/FullpageLoader'));
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Hero />}></Route>
-      <Route path='/profile' element={<Profile />} />
-      <Route path='/collection/:collectionName' element={<CollectionDetail/>}/>
-      <Route path='/Nft/:Nftname' element={<NftDetails/>}/>
-      <Route path='/Nft/:Nftname/buy' element={<BuyNft/>}/>
-      <Route path='/admin/login' element={<Login />} />
-      <Route path='/admin/*' element={<Admin />} />
-      <Route path='*' element={<PageNotFound />} />
-    </Routes>
+    <div>
+      <Suspense fallback={<FullpageLoader />}>
+        <Routes>
+          <Route path='/' element={<Hero />}></Route>
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/collection/:collectionName' element={<CollectionDetail/>}/>
+          <Route path='/Nft/:Nftname' element={<NftDetails/>}/>
+          <Route path='/Nft/:Nftname/buy' element={<BuyNft/>}/>
+          <Route path='/admin/login' element={<Login />} />
+          <Route path='/admin/*' element={<Admin />} />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+    </Suspense>
+    </div>
   );
 }
 
